398155ecc469e1e640e82f78526f5fa145226252
xiaoxiaopingzi <1641084175@qq.com>
2017-05-16 21:36:02 +0800

    day6 编写Web框架以及配置文件

--- www/requestHandler.py
+++ www/requestHandler.py
@@ -4,7 +4,10 @@ import asyncio
 import functools
 import inspect
 import logging
+from urllib import parse
 from aiohttp import web
+import os
+
 
 # 装饰器，用于获取GET提交的路径和参数
 def get(path):
@@ -41,6 +44,45 @@ def post(path):
 
     return decorator
 
+def get_required_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args = []
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+def has_named_kw_args(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.KEYWORD_ONLY:
+            return True
+
+def has_var_kw_arg(fn):
+    params = inspect.signature(fn).parameters
+    for name, param in params.items():
+        if param.kind == inspect.Parameter.VAR_KEYWORD:
+            return True
+
+def has_request_arg(fn):
+    sig = inspect.signature(fn)
+    params = sig.parameters
+    found = False
+    for name, param in params.items():
+        if name == 'request':
+            found = True
+            continue
+        if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+            raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+    return found
 
 # RequestHandler目的就是从URL处理函数（如handlers.index）中分析其需要接收的参数，从web.request对象中获取必要的参数，
 # 调用URL处理函数，然后把结果转换为web.Response对象，这样，就完全符合aiohttp框架的要求
@@ -48,11 +90,86 @@ class RequestHandler(object):
     def __init__(self, app, fn):
         self._app = app
         self._func = fn
+        self._has_request_arg = has_request_arg(fn)
+        self._has_var_kw_arg = has_var_kw_arg(fn)
+        self._has_named_kw_args = has_named_kw_args(fn)
+        self._named_kw_args = get_named_kw_args(fn)
+        self._required_kw_args = get_required_kw_args(fn)
 
+    # 1.定义kw对象，用于保存参数
+    # 2.判断URL处理函数是否存在参数，如果存在则根据是POST还是GET方法将request请求内容保存到kw
+    # 3.如果kw为空(说明request没有请求内容)，则将match_info列表里面的资源映射表赋值给kw；如果不为空则把命名关键字参数的内容给kw
+    # 4.完善_has_request_arg和_required_kw_args属性
     async def __call__(self, request):
-        kw = {}  # ... 获取参数
-        r = await self._func(**kw)
-        return r
+        kw = None
+        # 如果有需要处理的参数
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+            if request.method == 'POST':
+                if not request.content_type:
+                    return web.HTTPBadRequest('Missing Content-Type.')
+                ct = request.content_type.lower()
+                if ct.startswith('application/json'):
+                    params = await request.json()
+                    if not isinstance(params, dict):
+                        return web.HTTPBadRequest('JSON body must be object.')
+                    kw = params
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                    params = await request.post()
+                    kw = dict(**params)
+                else:
+                    return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
+            if request.method == 'GET':
+                qs = request.query_string
+                if qs:
+                    kw = dict()
+                    # 解析url中?后面的键值对内容保存到request_content
+                    '''
+                    qs = 'first=f,s&second=s'
+                    parse.parse_qs(qs, True).items()
+                    >>> dict([('first', ['f,s']), ('second', ['s'])])
+                    '''
+                    for k, v in parse.parse_qs(qs, True).items():
+                        kw[k] = v[0]
+        if kw is None:
+            # 参数为空说明没有从request对象中获取到参数,或者URL处理函数没有参数
+            '''
+            def hello(request):
+                    text = '<h1>hello, %s!</h1>' % request.match_info['name']
+                    return web.Response()
+            app.router.add_route('GET', '/hello/{name}', hello)
+            '''
+            '''if not self._has_var_kw_arg and not self._has_kw_arg and not self._required_kw_args:
+                # 当URL处理函数没有参数时，将request.match_info设为空，防止调用出错
+                request_content = dict()
+            '''
+            kw = dict(**request.match_info)
+        else:
+            if not self._has_var_kw_arg and self._named_kw_args:
+                # remove all unamed kw， 从request_content中删除URL处理函数中所有不需要的参数
+                copy = dict()
+                for name in self._named_kw_args:
+                    if name in kw:
+                        copy[name] = kw[name]
+                kw = copy
+            # check named arg: 检查关键字参数的名字是否和match_info中的重复
+            for k, v in request.match_info.items():
+                if k in kw:
+                    logging.warning('Duplicate arg name in named arg and kw args: %s' % k)
+                kw[k] = v
+        if self._has_request_arg:
+            kw['request'] = request
+        # check required kw: 检查是否有必须关键字参数
+        if self._required_kw_args:
+            for name in self._required_kw_args:
+                if not name in kw:
+                    return web.HTTPBadRequest('Missing argument: %s' % name)
+        # 以上代码均是为了获取调用参数
+        logging.info('call with args: %s' % str(kw))
+        try:
+            r = await self._func(**kw)
+            return r
+        except APIError as e:
+            return dict(error=e.error, data=e.data, message=e.message)
 
 
 def add_route(app, fn):
@@ -74,6 +191,12 @@ def add_route(app, fn):
     # 自省函数 '__call__'
     app.router.add_route(method, path, RequestHandler(app, fn))
 
+
+# 添加CSS等静态文件所在路径
+def add_static(app):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+    app.router.add_static('/static/', path)
+    logging.info('add static %s => %s' % ('/static/', path))
 
 def add_routes(app, module_name):
     # rfind()函数的作用是返回'.'在字符串module_name中最大的索引,返回-1则表示每找到'.'符号
